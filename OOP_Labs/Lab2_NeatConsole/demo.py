1f35874bfe249d3d63321a30e55cff79e49148a9
sum1ko05 <152371497+sum1ko05@users.noreply.github.com>
2025-04-19 18:40:59 +0200

    Minor addition + adoc update

--- OOP_Labs/Lab2_NeatConsole/demo.py
+++ OOP_Labs/Lab2_NeatConsole/demo.py
@@ -1,21 +1,24 @@
 from printer_base import Printer, Color
 
+
 common_font_path = "default.json"
 Printer.cls_print(color=Color.LightRed, 
                   position=(5, 5), 
                   font_path=common_font_path,
                   string="Simple demo")
 
-script = str("Hi my name is Reggie").split(" ")
-pallete = [Color.DarkYellow, 
-           Color.LightYellow,
+script = str("OwO what is this").split(" ")
+palette = [Color.LightYellow,
            Color.LightGreen,
            Color.LightBlue,
            Color.DarkPurple]
-printer = Printer(color=pallete[0], position=(0, 0), font_path=common_font_path)
+printer = Printer(color=palette[0], position=(0, 0), font_path=common_font_path)
 for i in range(len(script)):
     offset = i * 7
-    current_color = pallete[i % len(pallete)]
+    current_color = palette[i % len(palette)]
     printer.color = current_color
     printer.pos = (offset, 1)
     printer.print(script[i])
+with Printer(color=Color.LightCyan, position=(5, 1), font_path=common_font_path) as with_printer:
+    with_printer.print("notices")
+    with_printer.print("ur code")
--- OOP_Labs/Lab2_NeatConsole/printer_base.py
+++ OOP_Labs/Lab2_NeatConsole/printer_base.py
@@ -1,5 +1,7 @@
 from enum import Enum
 import json
+import os
+
 
 class Color(Enum):
     DarkBlack = "\033[0;30m"
@@ -22,12 +24,22 @@ class Color(Enum):
 
     Reset = "\033[0m"
 
+
 class Printer:
     def __init__(self, color: Color, position: tuple[int, int], font_path: str):
         self.color = color
         self.pos = position
         self.__read_font_file(font_path)
 
+    # region properties
+    @property
+    def color(self) -> Color:
+        return self._color
+    
+    @color.setter
+    def color(self, color: Color):
+        self._color = color
+    
     @property
     def pos(self) -> tuple[int, int]:
         return self._pos
@@ -35,22 +47,32 @@ class Printer:
     @pos.setter
     def pos(self, position: tuple[int, int]):
         if position[0] < 0:
-            raise(ValueError)
+            raise ValueError("Position is out of valid range")
         if position[1] < 0:
-            raise(ValueError)
+            raise ValueError("Position is out of valid range")
         self._pos = position
+    # endregion
 
-    def __read_font_file(self, font_json_path: str) -> None:
-        with open(font_json_path) as json_font:
-            items = json.load(json_font)
+    # region context_managing
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, *args):
+        pass
+    # endregion
+
+    # region low_level
+    def __read_font_file(self, font_json_path: str) -> None:  # It's possible to make it public to change font in middle of object lifetime
+        try:
+            with open(font_json_path) as json_font:
+                items = json.load(json_font)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File \"{font_json_path}\" not found in \"{os.getcwd()}\"")
         if items["metadata"]["version"] == 1.0:
             self._char_size = (items["metadata"]["char_width"], items["metadata"]["char_height"])
         self._chars = items["characters"]
-
-    def __to_font_char(self, char: str) -> list[str]:
-        return self._chars[char]
-    
-    def __prepare_string(self, string: str) -> str: # Preventing key errors
+ 
+    def __prepare_string(self, string: str) -> str:  # Preventing key errors
         result = string.upper()
         supported_chars = set(self._chars.keys())
         chars_in_str = set(result)
@@ -61,21 +83,23 @@ class Printer:
 
     def __render_text(self, string: str) -> list[str]:
         result = []
-        for i in range(self.pos[1]): # Making vertical offset
+        for i in range(self.pos[1]):  # Making vertical offset
             result.append("")
-        for i in range(self._char_size[1]): # Constructing strings from top to bottom
+        for i in range(self._char_size[1]):  # Constructing strings from top to bottom
             rendered_str = ""
-            rendered_str += self.color.value # Applying color to string
-            rendered_str += " " * self.pos[0] # Applying horizontal offset
+            rendered_str += self.color.value  # Applying color to string
+            rendered_str += " " * self.pos[0]  # Applying horizontal offset
             for char in string:
-                rendered_str += self._chars[char][i] + " " # After preparing text it's safe to check by index trust me
-            rendered_str += Color.Reset.value # Clean color after finishing writing
+                rendered_str += self._chars[char][i] + " "  # After preparing text it's safe to check by index trust me
+            rendered_str += Color.Reset.value  # Clean color after finishing writing
             result.append(rendered_str)
         return result
-    
-    def __out_to_console(self, render: list[str]) -> None:
+
+    @staticmethod
+    def __out_to_console(render: list[str]) -> None:
         for string in render:
             print(string)
+    # endregion
 
     @classmethod
     def cls_print(cls, color: Color, position: tuple[int, int], font_path: str, string: str):
@@ -85,8 +109,3 @@ class Printer:
         adapted_str = self.__prepare_string(string)
         rendered_str = self.__render_text(adapted_str)
         self.__out_to_console(rendered_str)
-
-# p = Printer(color=Color.LightPurple, position=(5, 2), font_path="default.json")
-# p.print("You are boykisser")
-# print("\033[31mHello\033[0m")
-# print(p.chars)
