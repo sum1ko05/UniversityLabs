1a9fa0888172645a3c62078a37665a3c7e776a1a
sum1ko05 <152371497+sum1ko05@users.noreply.github.com>
2026-02-12 12:28:36 +0200

    Finished stage 4

--- php-session-lab/www/process.php
+++ php-session-lab/www/process.php
@@ -2,6 +2,7 @@
 
 session_start();
 
+// Pull data from form
 $username = htmlspecialchars($_POST['username']);
 $email = htmlspecialchars($_POST['email'] ?? '');
 
@@ -9,6 +10,11 @@ $email = htmlspecialchars($_POST['email'] ?? '');
 $_SESSION['username'] = $username;
 $_SESSION['email'] = $email;
 
+// Write form data to external text file
+$line = $username . ";" . $email . "\n";
+file_put_contents("data.txt", $line, FILE_APPEND);
+
+// Redirect to index.php
 header("Location: index.php");
 exit();
 
