9ab4765ffb2dfa627702e73ae2fe916baff41c52
sum1ko05 <152371497+sum1ko05@users.noreply.github.com>
2026-02-12 11:17:05 +0200

    Finished stage 2

---
+++ php-session-lab/www/process.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+
+$username = htmlspecialchars($_POST['username']);
+$email = htmlspecialchars($_POST['email'] ?? '');
+
+// Save form data to session
+$_SESSION['username'] = $username;
+$_SESSION['email'] = $email;
+
+header("Location: index.php");
+exit();
+?>
