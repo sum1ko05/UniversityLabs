573fa0a1aea67856b5f4de26c9f333400d38bb6a
sum1ko05 <152371497+sum1ko05@users.noreply.github.com>
2026-03-05 12:20:12 +0200

    Finished preparations

---
+++ lab5/public/index.php
@@ -0,0 +1,36 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Все данные</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+    <div class="session_data">
+        <?php if(isset($_SESSION['username'])): ?>
+            <h2>Данные из сессии:</h2>
+            <ul>
+                <li>Имя: <?= $_SESSION['username'] ?></li>
+                <li>Email: <?= $_SESSION['email'] ?></li>
+            </ul>
+        <?php else: ?>
+            <p>Данных пока нет.</p>
+        <?php endif; ?>
+    </div>
+    <div class="errors">
+        <?php if(isset($_SESSION['errors'])): ?>
+            <h2>Данные не сохранены!</h2>
+            <ul>
+                <?php foreach($_SESSION['errors'] as $error): ?>
+                    <li><?= $error ?></li>
+                <?php endforeach; ?>
+            </ul>
+            <?php unset($_SESSION['errors']); ?>
+        <?php endif; ?>
+    </div>
+    <div class="form_and_view_links">
+        <a href="form.html">Заполнить форму</a> |
+        <a href="view.php">Посмотреть все данные</a>
+    </div>
+</body>
