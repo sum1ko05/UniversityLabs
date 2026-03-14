400332f538fca3aaea35c409d0d874ad7db78d5e
sum1ko05 <152371497+sum1ko05@users.noreply.github.com>
2026-03-14 15:32:21 +0200

    Finished minimal ElasticSearch setup

---
+++ lab6/public/classes/clients/ElasticClient.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+require 'vendor/autoload.php';
+require 'ClientFactory.php';
+
+use App\Helpers\ClientFactory;
+
+class ElasticClient
+{
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = ClientFactory::make('http://elasticsearch:9200/');
+    }
+
+    public function indexDocument($index, $id, $data)
+    {
+        $response = $this->client->put("$index/_doc/$id", [
+            'json' => $data
+        ]);
+        return $response->getBody()->getContents();
+    }
+
+    public function search($index, $query)
+    {
+        $response = $this->client->get("$index/_search", [
+            'json' => ['query' => ['match' => $query]]
+        ]);
+        return $response->getBody()->getContents();
+    }
+}
--- lab6/public/index.php
+++ lab6/public/index.php
@@ -1,2 +1,11 @@
 <?php
-echo("Project set up!");
+
+require 'vendor/autoload.php';
+require 'classes/clients/ElasticClient.php';
+
+use App\ElasticClient;
+
+// Elasticsearch
+$elastic = new ElasticClient();
+echo $elastic->indexDocument('books', 1, ['title' => '1984', 'author' => 'Orwell']);
+echo $elastic->search('books', ['author' => 'Orwell']);
