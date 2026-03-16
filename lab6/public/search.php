3588b6da68fb61dddef915ffab1c6eed52b1d7dc
sum1ko05 <152371497+sum1ko05@users.noreply.github.com>
2026-03-16 18:15:03 +0200

    Added basic title search

--- lab6/public/search.php
+++ lab6/public/search.php
@@ -7,28 +7,23 @@ use App\ElasticClient;
 use App\MoviesApiClient;
 
 $elastic = new ElasticClient();
-$moviesapi = new MoviesApiClient();
 
-$moviesapi_data = json_decode($moviesapi->get_all(), true)['data'];
-// Pass data from api to Elastic
-foreach($moviesapi_data as $entry)
-{
-    $entry_id = $entry['id'];
-    unset($entry['id']);
-    $elastic->indexDocument('movies', $entry_id, $entry);
-}
+$search_entry = htmlspecialchars($_POST['search_entry']);
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Study movie search</title>
+    <title>Study movie search - search</title>
     <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
-    <h1 class="website_title">Study movie search</h1>
+    <h3 class="small_website_title">SMS</h1>
     <form id="search_entry" action="search.php" method="POST">
         <input type="text" name="search_entry" placeholder="">
         <button type="submit"><i class="fa fa-search"></i></button>
     </form>
+    <?php
+        echo $elastic->search('movies', ['title' => $search_entry]);
+    ?>
 </body>
