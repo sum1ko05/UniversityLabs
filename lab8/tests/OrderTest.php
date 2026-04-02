09553784ec30761ea8262ae93dcb4ee2d83f4cc7
sum1ko05 <152371497+sum1ko05@users.noreply.github.com>
2026-04-02 17:09:19 +0200

    Finished step 3

--- lab8/tests/OrderTest.php
+++ lab8/tests/OrderTest.php
@@ -6,17 +6,25 @@ require_once __DIR__ . '/../public/classes/Order.php';
 
 class OrderTest extends TestCase
 {
-   public function testCreate()
+   public function testCreateWithMock()
    {
-       $order = new Order(null);
+        $stmt_mock = $this->createMock(PDOStatement::class);
+        $stmt_mock->method("execute")
+                  ->willReturn(true);
+    
+        $pdo_mock = $this->createMock(PDO::class);
+        $pdo_mock->method("prepare")
+                 ->willReturn($stmt_mock);
 
-       $result = $order->create("Ivan", 
-                                "ivan@test.com", 
-                                "chair", 
-                                1, 
-                                false, 
-                                "pickup_delivery");
+        $order = new Order($pdo_mock);
 
-       $this->assertEquals("Ivan's order created", $result);
+        $result = $order->create("Ivan", 
+                                 "ivan@snafu.com", 
+                                 "chair", 
+                                 1, 
+                                 0, 
+                                 "pickup_delivery");
+
+        $this->assertEquals(null, $result);
    }
 }
