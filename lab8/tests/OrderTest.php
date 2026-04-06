4aa2da89e6f1a90c7e009e1047e94ea25d8d0133
sum1ko05 <152371497+sum1ko05@users.noreply.github.com>
2026-04-06 11:42:30 +0200

    Fixed tests

--- lab8/tests/OrderTest.php
+++ lab8/tests/OrderTest.php
@@ -6,42 +6,49 @@ require_once __DIR__ . '/../public/classes/Order.php';
 
 class OrderTest extends TestCase
 {
+    private $stmt_mock;
     private $pdo_mock;
     private $order;
 
     protected function setUp(): void
     {
-        $stmt_mock = $this->createMock(PDOStatement::class);
-        $stmt_mock->method("execute")
-                         ->willReturn(true);
-    
+        $this->stmt_mock = $this->createMock(PDOStatement::class);
         $this->pdo_mock = $this->createMock(PDO::class);
-        $this->pdo_mock->method("prepare")
-                        ->willReturn($stmt_mock);
-        $this->pdo_mock->method("query")
-                        ->willReturn($stmt_mock);
-
-        #var_dump($this->$pdo_mock);
-        
         $this->order = new Order($this->pdo_mock);
     }
-   
+    
     public function testCreateWithMock()
     {
+        $this->stmt_mock->expects($this->once())
+                        ->method("execute")
+                        ->with(["Ivan", "ivan@snafu.com", "chair", 1, 0, "pickup_delivery"])
+                        ->willReturn(true);
+
+        $this->pdo_mock->expects($this->once())
+                       ->method("prepare")
+                       ->with("INSERT INTO orders (name, email, model, amount, assembly_included, delivery) VALUES (?, ?, ?, ?, ?, ?)")
+                       ->willReturn($this->stmt_mock);
+
         $result = $this->order->create("Ivan", 
                                        "ivan@snafu.com", 
                                        "chair", 
                                        1, 
                                        0, 
                                        "pickup_delivery");
-
-        #var_dump($result);
-
-        $this->assertEquals(null, $result);
     }
 
-    public function testReadAll()
+    public function testReadAllWithMock()
     {
+        $this->stmt_mock->expects($this->once())
+                        ->method("fetchAll")
+                        ->with()
+                        ->willReturn([]);
+
+        $this->pdo_mock->expects($this->once())
+                       ->method("query")
+                       ->with("SELECT * FROM orders")
+                       ->willReturn($this->stmt_mock);
+
         $result = $this->order->readAll();
 
         $this->assertEquals([], $result);
